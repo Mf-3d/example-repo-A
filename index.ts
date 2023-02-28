37ae0e364947535e57962de23c0aee6e15f9d50a
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2023-02-28 15:52:21 +0900

    🎉 初期化

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { EventEmitter } from 'node:events';
+import c from './c';
+export const event = new EventEmitter();
+
+let b: string = "b";
+
+export const d = c;
+
+/**
+ * 2つの数値を加算する関数
+ *
+ * @example
+ * ```js
+ * add(1, 2);
+ * 
+ * // -> 3
+ * ```
+ * @param {number} a 加算される数値
+ * @param {number} b 加算する数値
+ * @returns {number} 加算結果の数値
+ */
+export function add(a: number, b: number): number {
+  return a + b;
+}
